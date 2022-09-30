99f3f2cdc606e18c86f3b3edaa2cb3d86120a698
DESKTOP-FJD064R\min woo <minwocee@naver.com>
2022-09-30 14:17:51 +0900

    22.09.30

--- 1ㄱㄱ간단 연습장.py
+++ 1ㄱㄱ간단 연습장.py
@@ -1,38 +1,42 @@
+#if문 활용예제
+k=int(input("1부터 100사이의 정수를 입력 하시오: "))
 
-N,M=map(int, input().split())    #7명, 11일
-firstlist=list(map(int,input().split()))    #N명 만큼 초기 배치를 넣음(이 규칙대로 인덱스 참고) 5 6 2 1 7 3 4
+if k<100:
+    print("입력한 정수가 100보다 작음")
+    print(f"a = {k}")
 
-a = []    # 전체리스트속 리스트를 생성(이중 배열)
- 
-for i in range(M):
-    line = []              # 안쪽 리스트로 사용할 빈 리스트 생성
-    for j in range(N):
-        line.append(0)     # 안쪽 리스트에 0 추가
-    a.append(line)         # 전체 리스트에 안쪽 리스트를 추가
- 
-"""
-1일째: 5 6 2 1 7 3 4    (최초 선언 규칙)
-2일째: 7 3 6 5 4 2 1
-3일째: 4 2 3 7 1 6 5
-"""
+if k>=100:
+    print("프로그램 종료") 
 
-rulelist=[]
-for i in range(N):     #sl에 쓰레기값 넣음
-    rulelist.append(0)
+#예제 4-2 if-else 활용
 
-for i in range(N):     #tl에 쓰레기값 넣음
-    rulelist[i]=firstlist[i]
-print(rulelist, "움직이는 규칙들 정상적으로 삽입 완료")    #5 6 2 1 7 3 4
+k=int(input("1부터 100사이의 정수를 입력 하시오: "))
 
-for i in range(1,M):    #M=11 1,2,3,4...11
-    for p in range(N):
-        c=firstlist[p]
-        a[i][p]=rulelist[c-1]
-    
-    for q in range(N):    #룰 리스트를 업데이트
-        rulelist[q]=a[i][q]
-
-print(a[-1])
-   
+if k<100:
+    print(f"입력한 a의 값은 {k}이며 100보다 작음")
+    print("프로그램 종료") 
+else:
+     print(f"입력한 a의 값은 {k}이며 100보다 크다")
+     print("프로그램 종료") 
 
 
+#중복 if-else문 활용
+k=int(input("1부터 100사이의 정수를 입력 하시오: "))
+if k>=0:
+    if k==0:
+        print("입력한 정수는 0임")
+    else:
+        print("입력한 정수는 %d이고 양수임"%k)
+else:
+    print("입력한 정수는 %d이고 음수임"%k)
+
+#나이와 점수에 따라 합/불을 정하는 프로그램
+age, score=map(int,input().split())
+
+if age>20:
+    if score>80:
+        print("합격입니다")
+    else:
+        print("점수가 너무 낮아 불합격 입니다.")
+else:
+    print("나이가 너무 어려서 불합격 입니다.")
