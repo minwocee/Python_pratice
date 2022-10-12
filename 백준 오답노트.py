956983c110ef6207f309c65b92d44efa19630652
DESKTOP-FJD064R\min woo <minwocee@naver.com>
2022-10-13 02:14:15 +0900

    22.10.13

--- 백준 오답노트.py
+++ 백준 오답노트.py
@@ -250,3 +250,11 @@ for i in mlist:
         print(Total[i])
     except IndexError:
         continue
+
+
+# 백준 11654 아스키 코드 리턴
+# ord() : 문자의 아스키 코드값을 리턴하는 함수이다.
+# chr() : 아스키 코드값 입력으로 받아 그 코드에 해당하는 문자를 출력하는 함수이다
+
+N=input()
+print(ord(N))
