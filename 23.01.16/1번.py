9448a01e6649de4b59da795aff5a9e657800eb23
minwocee <minwocee@naver.com>
2023-01-19 21:12:53 +0900

    23.01.19

--- 23.01.16/1번.py
+++ 23.01.16/1번.py
@@ -1,21 +1,2 @@
-import sys
-
-N, L, D=map(int,sys.stdin.readline().split())  #곡개수, 곡길이, 전화벨 간격
-
-# 2 5 7
-
-# 총 15초 길이
-러닝타임=N*L + 5*(N-1)
-
-
-print(러닝타임)
-
-
-
-
-
-
-
-
-
-
+a = {1:2, 3:4, 2:45}
+print(list(a.keys())[0:3])
