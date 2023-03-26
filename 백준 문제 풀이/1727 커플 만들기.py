e19b9cfeabb123f1327b1a091ef965ab3a116800
minwocee <minwocee@naver.com>
2023-03-26 16:07:41 +0900

    23.03.26

--- 백준 문제 풀이/1727 커플 만들기.py
+++ 백준 문제 풀이/1727 커플 만들기.py
@@ -1,4 +1,11 @@
 # https://www.acmicpc.net/problem/1727
 
+'''
+성격 수치는 1 ~ 10^6 이하의 자연수 이다.
+n,m은 1000 이하임, 10^6이므로 
+'''
 
+import sys
 
+n,m=map(int,sys.stdin.readline().split())   # 남자수, 여자수 입력 받음
+# 어케 풀지
